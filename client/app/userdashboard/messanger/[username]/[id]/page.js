019ea340e445854c99eb5252f23ebeaa8ab2afbb
Mirazul3221 <mirazul3221@gmail.com>
2024-11-16 18:25:47 +0600

    change messanger layout

--- client/app/userdashboard/messanger/[username]/[id]/page.js
+++ client/app/userdashboard/messanger/[username]/[id]/page.js
@@ -7,7 +7,6 @@ import Middle from "../../components/MessageContainerMiddle";
 import { useEffect } from "react";
 import axios from "axios";
 import { baseurl } from "@/app/config";
-import EntryPoint from "@/app/userdashboard/components/messanger/video-audio-callcenter/EntryPoint";
 import { useContext } from "react";
 import storeContext from "@/app/global/createContex";
 import "@/app/userdashboard/components/cssfiles/scrolling_bar.css";
@@ -15,8 +14,6 @@ const Page = () => {
   const { store } = useContext(storeContext);
   const [userDetails, setUserDetails] = useState(null);
   const [userProfile, setUserProfile] = useState(null);
-  const [message, setMessage] = useState("");
-  const messangerRef = useRef(null);
   const path = useParams();
   const [id, setId] = useState(path.id);
   const fetchUser = async (id) => {
@@ -28,18 +25,6 @@ const Page = () => {
   useEffect(() => {
     fetchUser(id);
   }, [id]);
-
-  const handleMessage = (event) => {
-    setMessage(event.target.value);
-  };
-
-  useEffect(() => {
-    // messangerRef.current.addEventListener("keyUp",()=>alert("helo"))
-    if (messangerRef.current) {
-      messangerRef.current.style.height = "auto";
-      messangerRef.current.style.height = `${messangerRef.current.scrollHeight}px`;
-    }
-  }, [message]);
   return (
     <div className=" w-screen h-screen fixed top-0 left-0">
       <div className="border-b bg-white px-8 py-2">
@@ -61,92 +46,26 @@ const Page = () => {
             <MessageBox setId={setId} setUserDetails={setUserDetails} />
           </div>
         </div>
-        <div className="middle w-6/12 border rounded-2xl overflow-y-scroll  hidden_scroll h-[81vh]">
-          <div className="top-bar px-4 rounded-t-2xl py-2 bg-violet-500 flex justify-between items-center">
-            <div className="flex gap-3 items-center">
-              <img
-                className="rounded-full w-12"
-                src={userProfile?.profile}
-                alt={userProfile?.name}
-              />
-              <div className="text-white">
-                <h2>{userProfile?.name}</h2>
-                <p className="text-[12px]">{userProfile?.status}</p>
-              </div>
-            </div>
-            <div className="flex justify-center gap-4 text-white">
-              <EntryPoint
-                user={{
-                  myId: store.userInfo.id,
-                  fdId: userProfile?._id,
-                  name: userProfile?.name,
-                  profile: userProfile?.profile,
-                  title: userProfile?.title,
-                  type: "Audio",
-                  size: 35,
-                }}
-              />
-              <EntryPoint
-                user={{
-                  myId: store.userInfo.id,
-                  fdId: userProfile?._id,
-                  name: userProfile?.name,
-                  profile: userProfile?.profile,
-                  title: userProfile?.title,
-                  type: "Video",
-                  size: 35,
-                }}
-              />
-            </div>
-          </div>
-          <div>
-            <div className=" bg-white border-gray-300">
-              <Middle id={id} userDetails={userProfile} />
-            </div>
-            <div className={"bottom"}>
-               <div className="p-4 flex justify-between items-center gap-2">
-               <textarea
-                  ref={messangerRef}
-                  value={message}
-                  onMouseUp={() => setLeftHide(true)}
-                  onChange={(e) => handleMessage(e)}
-                  rows={1}
-                  style={{
-                    width: "100%",
-                    resize: "none",
-                    overflow: "hidden",
-                    padding: "6px 14px",
-                    boxSizing: "border-box",
-                    outline: "none",
-                    border: "none",
-                    borderRadius: "20px",
-                    background: "#ededed",
-                  }}
-                />
-
-                <div>
-                  dgdsgs
-                </div>
-               </div>
-              </div>
+        <div className="border middle rounded-2xl middle w-6/12 h-[82vh]">
+          <div className=" bg-white border-gray-300">
+            <Middle id={id} userDetails={userProfile} />
           </div>
         </div>
         <div className="write w-3/12 bg-white">
-        <div className="overflow-y-auto h-20">
-          gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
-          gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
-          gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
-          gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
-          gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
-          gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
-          gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
-          gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
-          gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
-          gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
-          gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
-          gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
-        </div>
-        
+          <div className="overflow-y-auto h-20">
+            gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
+            gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
+            gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
+            gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
+            gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
+            gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
+            gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
+            gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
+            gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
+            gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
+            gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
+            gsdrrrrrrrrrrr dtttttttttttttt jttttttttttttt jtttttttttttttttttttt
+          </div>
         </div>
       </div>
     </div>
--- client/app/userdashboard/messanger/components/MessageContainerMiddle.jsx
+++ client/app/userdashboard/messanger/components/MessageContainerMiddle.jsx
@@ -1,14 +1,19 @@
-'use client'
+"use client";
 import { baseurl } from "@/app/config";
 import storeContext from "@/app/global/createContex";
 import axios from "axios";
-import React, { useContext} from "react";
+import React, { useContext, useRef, useState } from "react";
 import { useEffect } from "react";
 import { groupMessagesBysender } from "./group-message";
 import { useMessage } from "../../global/messageProvider";
 import "@/app/userdashboard/components/cssfiles/scrolling_bar.css";
-const Middle = ({ id,userDetails }) => {
-  const {messages,dispatch} = useMessage()
+import { RiSendPlaneLine } from "react-icons/ri";
+import EntryPoint from "../../components/messanger/video-audio-callcenter/EntryPoint";
+const Middle = ({ id, userDetails }) => {
+  const { messages, dispatch } = useMessage();
+  const [message, setMessage] = useState("");
+  const messangerRef = useRef(null);
+  const scrollRef = useRef();
   const { store } = useContext(storeContext);
   useEffect(() => {
     async function fetchMessage() {
@@ -18,56 +23,113 @@ const Middle = ({ id,userDetails }) => {
             Authorization: `Bearer ${store.token}`,
           },
         });
-        dispatch({type:'fetch-message',payload:data})
+        dispatch({ type: "fetch-message", payload: data });
       } catch (error) {}
     }
     fetchMessage();
   }, [id]);
   const groupMessages = groupMessagesBysender(messages);
 
+  const handleMessage = (event) => {
+    setMessage(event.target.value);
+  };
+  useEffect(() => {
+    // messangerRef.current.addEventListener("keyUp",()=>alert("helo"))
+    if (messangerRef.current) {
+      messangerRef.current.style.height = "auto";
+      messangerRef.current.style.height = `${messangerRef.current.scrollHeight}px`;
+    }
+  }, [message]);
   return (
-    <div className="w-full overflow-y-auto hidden_scroll h-[60vh] py-6 px-4 bg-white">
-      <div className="flex justify-center">
-         <div>
-         <img className="rounded-full w-28 mx-auto" src={userDetails?.profile} alt={userDetails?.name} />
-         <h4 className="text-center text-gray-700 text-2xl font-semibold">{userDetails?.name}</h4>
-         </div>
-      </div>
-      {groupMessages?.map((messageBlog, i) => {
-        return (
-          <div key={i}>
-            {messageBlog.map((msg, i) => {
-              return (
-                <div  key={i}
-                  className={`flex ${
-                    msg?.senderId === store.userInfo.id ? "justify-end" : ""
-                  }`}
-                >
-                  <div className="max-w-[60%] mb-[1px]">
-                    <h1
-                      className={`w-full 
+<div className="">
+<div className="top-bar px-4 rounded-t-2xl py-2 bg-violet-500 flex justify-between items-center">
+            <div className="flex gap-3 items-center">
+              <img
+                className="rounded-full w-12"
+                src={userDetails?.profile}
+                alt={userDetails?.name}
+              />
+              <div className="text-white">
+                <h2>{userDetails?.name}</h2>
+                <p className="text-[12px]">{userDetails?.status}</p>
+              </div>
+            </div>
+            <div className="flex justify-center gap-4 text-white">
+              <EntryPoint
+                user={{
+                  myId: store.userInfo.id,
+                  fdId: userDetails?._id,
+                  name: userDetails?.name,
+                  profile: userDetails?.profile,
+                  title: userDetails?.title,
+                  type: "Audio",
+                  size: 35,
+                }}
+              />
+              <EntryPoint
+                user={{
+                  myId: store.userInfo.id,
+                  fdId: userDetails?._id,
+                  name: userDetails?.name,
+                  profile: userDetails?.profile,
+                  title: userDetails?.title,
+                  type: "Video",
+                  size: 35,
+                }}
+              />
+            </div>
+          </div>
+<div className=" overflow-y-scroll  hidden_scroll h-[71vh]">
+      <div className="w-full overflow-y-auto hidden_scroll h-[60vh] py-6 px-4 bg-white">
+        <div className="flex justify-center">
+          <div>
+            <img
+              className="rounded-full w-28 mx-auto"
+              src={userDetails?.profile}
+              alt={userDetails?.name}
+            />
+            <h4 className="text-center text-gray-700 text-2xl font-semibold">
+              {userDetails?.name}
+            </h4>
+          </div>
+        </div>
+        {groupMessages?.map((messageBlog, i) => {
+          return (
+            <div key={i}>
+              {messageBlog.map((msg, i) => {
+                return (
+                  <div
+                    key={i}
+                    className={`flex ${
+                      msg?.senderId === store.userInfo.id ? "justify-end" : ""
+                    }`}
+                  >
+                    <div className="max-w-[60%] mb-[1px]">
+                      <h1
+                        ref={scrollRef}
+                        className={`w-full 
                           ${messageBlog.length === 1 ? "rounded-[30px]" : ""}
                         ${
-                        msg?.senderId === store.userInfo.id
-                          ? "rounded-l-[30px]"
-                          : "rounded-r-[30px]"
-                      } px-5 py-2
+                          msg?.senderId === store.userInfo.id
+                            ? "rounded-l-[30px]"
+                            : "rounded-r-[30px]"
+                        } px-5 py-2
                     ${
                       msg?.senderId === store.userInfo.id
                         ? "bg-gray-100 text-right text-gray-700"
                         : "bg-violet-500 text-white"
                     } ${
-                        messageBlog.indexOf(msg) === 0 &&
-                        messageBlog.length !== 1 &&
-                        msg?.senderId !== store.userInfo.id
-                          ? "rounded-tl-[30px]"
-                          : messageBlog.indexOf(msg) ===
-                              messageBlog.length - 1 &&
-                            messageBlog.length !==1 &&
-                            msg?.senderId !== store.userInfo.id
-                          ? "rounded-bl-[30px]"
-                          : ""
-                      } 
+                          messageBlog.indexOf(msg) === 0 &&
+                          messageBlog.length !== 1 &&
+                          msg?.senderId !== store.userInfo.id
+                            ? "rounded-tl-[30px]"
+                            : messageBlog.indexOf(msg) ===
+                                messageBlog.length - 1 &&
+                              messageBlog.length !== 1 &&
+                              msg?.senderId !== store.userInfo.id
+                            ? "rounded-bl-[30px]"
+                            : ""
+                        } 
                      ${
                        messageBlog.indexOf(msg) === 0 &&
                        messageBlog.length !== 1 &&
@@ -81,19 +143,55 @@ const Middle = ({ id,userDetails }) => {
                          : ""
                      }
                     `}
-                    >
-                      {msg?.message}
-                    </h1>
-                    {messageBlog.indexOf(msg) === messageBlog.length - 1 &&
-                      messageBlog.length > 0 && msg?.senderId !== store.userInfo.id && <div><img src=""/></div>}
+                      >
+                        {msg?.message}
+                      </h1>
+                      {messageBlog.indexOf(msg) === messageBlog.length - 1 &&
+                        messageBlog.length > 0 &&
+                        msg?.senderId !== store.userInfo.id && (
+                          <div>
+                            <img src="" />
+                          </div>
+                        )}
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
+          );
+        })}
+
+        {message && <h2>{message}</h2>}
+      </div>
+
+      {/* /////////////////////////////////////////////////////////////////////////////////////////// */}
+      <div className={"bottom"}>
+        <div className="p-4 flex justify-between items-end gap-2">
+          <textarea
+            ref={messangerRef}
+            value={message}
+            onChange={(e) => handleMessage(e)}
+            rows={1}
+            style={{
+              width: "100%",
+              resize: "none",
+              overflow: "hidden",
+              padding: "6px 14px",
+              boxSizing: "border-box",
+              outline: "none",
+              border: "none",
+              borderRadius: "20px",
+              background: "#ededed",
+            }}
+          />
+
+          <div className="flex h-full items-start">
+            <RiSendPlaneLine />
           </div>
-        );
-      })}
+        </div>
+      </div>
     </div>
+</div>
   );
 };
 
