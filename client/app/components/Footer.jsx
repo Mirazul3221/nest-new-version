2866beb5f3e63e4e83df17f8012f46d4b5363878
Mirazul3221 <mirazul3221@gmail.com>
2024-11-18 23:51:18 +0600

    Update online alert

--- client/app/components/Footer.jsx
+++ client/app/components/Footer.jsx
@@ -7,11 +7,15 @@ const Footer = () => {
     <div>
             <Banner/>
       <div className="footer flex justify-between items-center py-4 md:px-20 px-4 bg-gray-300">
-         <div className="flex gap-1 items-center">
+         <div className="md:flex gap-1 items-center">
          <h2>
           © Copyright 2024 | All Rights Reserved | Powered by Mirazul islam | 
         </h2>
         <h1>Bcs preparation online platform</h1>
+         </div>
+         <div className="flex gap-2">
+           <a href="/register">Sign Up</a>
+           <a href="/login">Log In</a>
          </div>
         <div className="flex items-center md:gap-4 gap-2">
           <FaFacebook size={25} />
