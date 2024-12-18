ffc4ed158c958ab21c265cfe2ca723f6588eb2fe
Mirazul3221 <mirazul3221@gmail.com>
2024-12-18 21:42:47 +0600

    fixed comment section

--- client/app/userdashboard/components/notification-component/QuestionCommentCard.jsx
+++ client/app/userdashboard/components/notification-component/QuestionCommentCard.jsx
@@ -1,13 +1,16 @@
 import { viewurl } from "@/app/config";
 import React from "react";
 import { FaComment } from "react-icons/fa";
+import { formatRelativeTime } from "../../timeline/components/common";
 const QuestionCommentCard = ({ item }) => {
+  console.log(item)
   return (
     <div>
-      <div className="py-2 border-b">
+      <div className="py-2 border-b px-2">
         <a href={`${viewurl}/userdashboard/timeline/${item?.message[0]?.slug}`}>
+          <span>{formatRelativeTime(item.createdAt)}</span>
           <div className="flex gap-4">
-            <div className="w-20 relative text-gray-700">
+            <div className="w-20 h-20 relative text-gray-700">
               <FaComment
                 className="absolute right-0 bottom-0"
                 color="#7305fa"
@@ -23,8 +26,8 @@ const QuestionCommentCard = ({ item }) => {
               <h2>
                 <span className="font-bold text-[#7305fa]">
                   {item?.message[0]?.requesterName}
-                </span>{" "}
-                comments {`"${item?.message[0]?.comment}"`} your question {` "${item?.message[0]?.question}"`}
+                </span> {' '}
+                comments as {`"${item?.message[0]?.Comment}"`} on your question {` "${item?.message[0]?.question}"`}
               </h2>
             </div>
           </div>
