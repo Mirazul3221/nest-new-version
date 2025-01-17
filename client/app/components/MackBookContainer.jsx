893b29d555b65d3559ddd5c0b51f9b59bd879e58
Mirazul3221 <mirazul3221@gmail.com>
2025-01-17 23:37:42 +0600

    Add threeD object from here 1

--- client/app/components/MackBookContainer.jsx
+++ client/app/components/MackBookContainer.jsx
@@ -7,13 +7,24 @@ const MackBookContainer = () => {
   return (
     <div className="w-full h-full">
            <Suspense fallback={<div className="text-white text-lg text-center mt-60"><h2>Loading 3D object...</h2></div>}>
-      <Canvas>
+           <div className="hidden md:block w-full h-full">
+           <Canvas>
             <Stage environment={'night'} intensity={0.5}>
             <MacBook />
             </Stage>
           <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={-1}/>
           <PerspectiveCamera position={[-1,0,1.8]} zoom={0.7} makeDefault/>
       </Canvas>
+           </div>
+           <div className="md:hidden w-full h-full">
+           <Canvas>
+            <Stage environment={'night'} intensity={0.5}>
+            <MacBook />
+            </Stage>
+          <OrbitControls  enableRotate={false} enableZoom={false} autoRotate autoRotateSpeed={-1}/>
+          <PerspectiveCamera position={[-.5,0,1.8]} zoom={0.7} makeDefault/>
+      </Canvas>
+           </div>
       </Suspense>
     </div>
   );
