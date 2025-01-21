3bf8486620719b32ce2c89a6d9b245db04dfb59d
Mirazul3221 <mirazul3221@gmail.com>
2025-01-22 04:26:20 +0600

    one blog post page done

--- client/app/blogs/static/how-to-prepare-myself-for-bcs-and-other-job/layout.js
+++ client/app/blogs/static/how-to-prepare-myself-for-bcs-and-other-job/layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 export const metadata = {
     title:'best BCS preparation',
-     keywords : 'how to prepare for bcs, best bcs preoaration guidline, my bcs preparation',
+     keywords:"BCS preparation, বিসিএস প্রস্তুতি, Bangladesh Civil Service, চাকরি প্রস্তুতি, প্রতিযোগিতামূলক পরীক্ষা",
     description: "বিসিএস পরীক্ষা বাংলাদেশের সবচেয়ে প্রতিযোগিতামূলক পরীক্ষাগুলোর মধ্যে অন্যতম। এটি শুধু একটি পরীক্ষা নয়, বরং একজন প্রার্থীর জীবনের দৃষ্টিভঙ্গি",
   };
 const Layout = ({children}) => {
