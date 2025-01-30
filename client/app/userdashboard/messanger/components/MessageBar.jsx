af774ab6a483437d5482206f17197eaafc8b2657
Mirazul3221 <mirazul3221@gmail.com>
2025-01-30 20:44:40 +0600

    just message seen icon hidden by click

---
+++ client/app/userdashboard/messanger/components/MessageBar.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react'
+import { useSocket } from '../../global/SocketProvider';
+import { useStore } from '@/app/global/DataProvider';
+import { viewurl } from '@/app/config';
+import { formatetime } from '../../components/messanger/components/time';
+
+const MessageBar = ({setId,setUserDetails,friend}) => {
+    const {socket, myActiveFriends} = useSocket();   
+      const { store } = useStore();
+//////////////////////////////typing effect///////////////////////////////////
+const [typing, setTyping] = useState("");
+useEffect(() => {
+  socket &&
+    socket.on("typingalert", (data) => {
+      setTyping(data);
+    });
+  return () => {
+    socket && socket.off("getTypingMsg");
+  };
+}, [socket]);
+/////////////////////////////////re-render users//////////////////////////////////
+useEffect(() => {
+  socket &&
+    socket.on("message-from", () => {
+      async function loadmessage() {
+        const data = await fetchAllFriendsByMessage(store.token);
+        setMessangerFriends(data);
+      }
+      loadmessage();    
+    });
+  return () => {
+    socket && socket.off("message-from");
+  };
+}, [socket]);
+
+const handleUrl = () => {
+    window.history.pushState(null, '', `${viewurl}/userdashboard/messanger/${friend.userName}/${friend.userId}`);
+    setId(friend.userId)
+    setUserDetails(friend)
+   // 
+  }
+  const [hasSeen,setHasSeen] = useState(false);
+
+  const updateUnseenMessage =async () => {
+    console.log('comming soon')
+  }
+  return (
+    <div onClick={()=> {
+        handleUrl()
+        setHasSeen(true)
+      }}>
+          <div className="px-6 flex gap-4 relative items-center rounded-2xl py-2 border-b hover:bg-gray-200 duration-100">
+            <div className="relative">
+              <img
+                className="w-12 rounded-full"
+                src={friend.userProfile}
+                alt={friend.userName}
+              />
+              {
+                myActiveFriends?.includes(friend.userId) &&   <div className="w-3 h-3 border-2 border-white bg-green-500 absolute rounded-full -right-[2px] bottom-1"></div>
+              }
+            </div>
+            <div>
+                <div className="flex gap-4 items-center justify-between">
+                <h2 className="text-lg font-semibold text-slate-700">
+                {friend.userName.split(' ')[0]}
+              </h2>
+              <p className="text-[12px] text-slate-500">
+                  {formatetime(friend.lastMessageTime)}
+                </p>
+                </div>
+                <h4 className="text-sm text-slate-500">
+                  <span className="font-semibold text-slate-700">
+                    {friend.senderId === store.userInfo.id ? "You :" : ""}
+                  </span>
+                 {
+                  typing.message && typing.senderId === friend.userId ? "Typing..." : friend.lastMessage.length > 20 ? friend.lastMessage.content.slice(0,20) + '......' : friend.lastMessage.content
+                 }
+                </h4>
+            </div>
+           {
+            !hasSeen &&  <div className="absolute top-[50%] -translate-y-[50%] right-5 bg-rose-100 text-gray-700 p-1 w-4 h-4 flex justify-center items-center rounded-full shadow-md text-[10px]">{friend?.unseenMessageCount}</div>
+           }
+          </div>
+      </div>
+  )
+}
+
+export default MessageBar
--- client/app/userdashboard/messanger/components/MessageContainerLeft.jsx
+++ client/app/userdashboard/messanger/components/MessageContainerLeft.jsx
@@ -1,15 +1,12 @@
 "use client";
-import { baseurl, viewurl } from "@/app/config";
-import storeContext from "@/app/global/createContex";
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import { fetchAllFriendsByMessage } from "./fetchdata";
-import { formatetime } from "../../components/messanger/components/time";
-import { useSocket } from "../../global/SocketProvider";
+import { useStore } from "@/app/global/DataProvider";
+import MessageBar from "./MessageBar";
 
 const MessageBox = ({setId,setUserDetails}) => {
-  const { store } = useContext(storeContext);
-  const {socket, myActiveFriends} = useSocket();
+  const { store } = useStore();
   const [messangerFriends, setMessangerFriends] = useState(null);
   useEffect(() => {
     async function loadmessage() {
@@ -21,77 +18,12 @@ const MessageBox = ({setId,setUserDetails}) => {
 
 
   const sortedMessages = messangerFriends?.sort((a, b) => new Date(b.lastMessageTime) - new Date(a.lastMessageTime));
-const handleUrl = (friend) => {
-  window.history.pushState(null, '', `${viewurl}/userdashboard/messanger/${friend.userName}/${friend.userId}`);
-  setId(friend.userId)
-  setUserDetails(friend)
- // 
-}
-//////////////////////////////typing effect///////////////////////////////////
-const [typing, setTyping] = useState("");
-useEffect(() => {
-  socket &&
-    socket.on("typingalert", (data) => {
-      setTyping(data);
-    });
-  return () => {
-    socket && socket.off("getTypingMsg");
-  };
-}, [socket]);
-/////////////////////////////////re-render users//////////////////////////////////
-useEffect(() => {
-  socket &&
-    socket.on("message-from", () => {
-      async function loadmessage() {
-        const data = await fetchAllFriendsByMessage(store.token);
-        setMessangerFriends(data);
-      }
-      loadmessage();    
-    });
-  return () => {
-    socket && socket.off("message-from");
-  };
-}, [socket]);
   return (
     <div className="w-full bg-white cursor-pointer">
       {sortedMessages &&
         sortedMessages.map((friend, i) => {
           return (
-            <div onClick={()=> {
-              handleUrl(friend)
-            }} key={i}>
-                <div className="px-6 flex gap-4 relative items-center rounded-2xl py-2 border-b hover:bg-gray-200 duration-100">
-                  <div className="relative">
-                    <img
-                      className="w-12 rounded-full"
-                      src={friend.userProfile}
-                      alt={friend.userName}
-                    />
-                    {
-                      myActiveFriends?.includes(friend.userId) &&   <div className="w-3 h-3 border-2 border-white bg-green-500 absolute rounded-full -right-[2px] bottom-1"></div>
-                    }
-                  </div>
-                  <div>
-                      <div className="flex gap-4 items-center justify-between">
-                      <h2 className="text-lg font-semibold text-slate-700">
-                      {friend.userName.split(' ')[0]}
-                    </h2>
-                    <p className="text-[12px] text-slate-500">
-                        {formatetime(friend.lastMessageTime)}
-                      </p>
-                      </div>
-                      <h4 className="text-sm text-slate-500">
-                        <span className="font-semibold text-slate-700">
-                          {friend.senderId === store.userInfo.id ? "You :" : ""}
-                        </span>
-                       {
-                        typing.message && typing.senderId === friend.userId ? "Typing..." : friend.lastMessage.length > 20 ? friend.lastMessage.content.slice(0,20) + '......' : friend.lastMessage.content
-                       }
-                      </h4>
-                  </div>
-                  <div className="absolute top-[50%] -translate-y-[50%] right-5 bg-rose-100 text-gray-700 p-1 w-4 h-4 flex justify-center items-center rounded-full shadow-md text-[10px]">{friend?.unseenMessageCount}</div>
-                </div>
-            </div>
+             <MessageBar setId={setId} setUserDetails={setUserDetails} key={i} friend={friend}/>
           );
         })}
     </div>
