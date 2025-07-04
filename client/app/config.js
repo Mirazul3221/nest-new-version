bf601580189db9aa2a77a3f65f86c99619eb648e
Mirazul3221 <mirazul3221@gmail.com>
2025-07-04 14:33:01 +0600

    Update cross btn

--- client/app/config.js
+++ client/app/config.js
@@ -6,6 +6,6 @@ export let baseurl01 = 'https://template-engine-u1xl.onrender.com';
 //==========MyPass = @43gnertnr%&Yjk%(&(
 //====================//====================
 export let viewurl = 'https://bcs-prep.vercel.app';
-viewurl = 'http://localhost:3000'
+// viewurl = 'http://localhost:3000'
 export const PUBLIC_VAPID_PUBLIC_KEY = 'BOUKJoJAzEl0IN4CMXNhgkQd_L8BySr3wnZM1crM9l2Wgbpr5tQbo3VzjJ2_tV5swA2iVerkkkJtPr0NUyjtW3o';
 
