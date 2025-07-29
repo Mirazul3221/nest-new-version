2da7cc584ba6ad1aeadea120895c34f0c75df3c1
Mirazul <mirazul3221@gmail.com>
2025-07-30 00:59:40 +0600

    First setup commit

--- client/app/config.js
+++ client/app/config.js
@@ -1,10 +1,9 @@
 export let baseurl = 'https://dev000.vercel.app';
-// baseurl = 'http://localhost:5050'
+baseurl = 'http://localhost:5050'
 export let baseurl01 = 'https://template-engine-u1xl.onrender.com';
 // baseurl01 = 'http://localhost:3300';
 //==========MyPass = @43gnertnr%&Yjk%(&(
 //====================//====================
 export let viewurl = 'https://bcs-prep.vercel.app';
-// viewurl = 'http://localhost:3000'
-export const PUBLIC_VAPID_PUBLIC_KEY = 'BOUKJoJAzEl0IN4CMXNhgkQd_L8BySr3wnZM1crM9l2Wgbpr5tQbo3VzjJ2_tV5swA2iVerkkkJtPr0NUyjtW3o';
-
+viewurl = 'http://localhost:3000'
+export const PUBLIC_VAPID_PUBLIC_KEY = 'BNaHWQXbQw07wgw5z65iP34OBIZHdSSUR0IavyIQn9BdO0JgAh1lJ1brGcSh_nsHj-vxPn4AP7mNHGLs9O3W1tw';
