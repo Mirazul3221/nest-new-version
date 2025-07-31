28015253c2f2e88fbeb73f8ed0525f2b042cd19d
Mirazul <mirazul3221@gmail.com>
2025-07-31 20:21:53 +0600

    Update code again

--- client/app/config.js
+++ client/app/config.js
@@ -6,4 +6,4 @@ export let baseurl01 = 'https://template-engine-u1xl.onrender.com';
 //====================//====================
 export let viewurl = 'https://bcs-prep.vercel.app';
 // viewurl = 'http://localhost:3000'
-export const PUBLIC_VAPID_PUBLIC_KEY = 'BCzHITyXyFkbknCBXG25-r71u0z2deZYMF_hsvd_TY3_r9G3HhZDYx2LtePNO_4v35unW5_E3d2h1_ictb-V8NY';
+export const PUBLIC_VAPID_PUBLIC_KEY = 'BCzHITyXyFkbknCBXG25-r71u0z2deZYMF_hsvd_TY3_r9G3HhZDYx2LtePNO_4v35unW5_E3d2h1_ictb-V8NY';//
