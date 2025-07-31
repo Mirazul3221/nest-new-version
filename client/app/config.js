0835707c1e9f29e47f64b4d46c53d617c3013424
Mirazul <mirazul3221@gmail.com>
2025-07-31 20:16:40 +0600

    Update code again

--- client/app/config.js
+++ client/app/config.js
@@ -1,9 +1,9 @@
 export let baseurl = 'https://dev0000.vercel.app';
-baseurl = 'http://localhost:5050'
+// baseurl = 'http://localhost:5050'
 export let baseurl01 = 'https://template-engine-u1xl.onrender.com';
 // baseurl01 = 'http://localhost:3300';
 //==========MyPass = @43gnertnr%&Yjk%(&(
 //====================//====================
 export let viewurl = 'https://bcs-prep.vercel.app';
-viewurl = 'http://localhost:3000'
+// viewurl = 'http://localhost:3000'
 export const PUBLIC_VAPID_PUBLIC_KEY = 'BCzHITyXyFkbknCBXG25-r71u0z2deZYMF_hsvd_TY3_r9G3HhZDYx2LtePNO_4v35unW5_E3d2h1_ictb-V8NY';
