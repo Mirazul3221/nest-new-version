1a5f493d6d4d790b636188c02d09548eb991d273
Mirazul <mirazul3221@gmail.com>
2025-09-14 11:42:32 +0600

    Modify PWA

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,11 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: false,
-    transpilePackages: ['three'],
-    eslint: {
-        ignoreDuringBuilds: true,
-      },
+import withPWA from "next-pwa";
 
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = withPWA({
+  reactStrictMode: false,
+  transpilePackages: ["three"],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    optimizeFonts: false, // enable font optimization
+  },
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development", // PWA disabled in dev
+  },
+});
 
 export default nextConfig;
